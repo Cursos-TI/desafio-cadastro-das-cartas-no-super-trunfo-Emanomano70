08f699d1d29763538321ba7e80731ca3776b33b1
Emanomano70 <e.souzavieira@hotmail.com>
2025-03-27 16:32:01 +0000

    savepoint

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -6,62 +6,85 @@
 // Siga os comentários para implementar cada parte do desafio.
 //Teste larissa
 
+
+
 int main() {
     printf("Desafio SuperTrunfo!\n");
     printf("Novo commit\n");
 
-    char estado_1,estado_2;
-    char codigo_1[20],codigo_2[20];
-    char cidade_1[50],cidade_2[50];
-    int populacao_1,populacao_2;
-    float area_1,area_2;
-    float pib_1,pib_2;
-    int pontosturisticos_1,pontosturisticos_2;
+//Dados da Carta 1
+
+    char estado1;
+    char codigo1[20];
+    char cidade1[50];
+    int populacao1;
+    float area1;
+    float pib1;
+    int pontosturisticos1;
+
+    //Dados da Carta 2
+
+    char estado2;
+    char codigo2[20];
+    char cidade2[50];
+    int populacao2;
+    float area2;
+    float pib2;
+    int pontosturisticos2;
+
+    //Entrada e armazenamento das informaçoes das cartas 1 e 2.
+
 
     printf("***Escreva os dados da Carta 1***\n");
     
 
     printf("Digite a letra do Estado (Entre A e H): \n");
-    scanf("%s", &estado_1);
+    scanf("%c", &estado1);
 
     printf("Digite o código da carta (ex:C01): \n");
-    scanf("%s", codigo_1);
+    scanf("%s", codigo1);
 
-    printf("Digite o nome da cidade: \n");
-   scanf("%s", cidade_1);
+    printf("Digite o nome da cidade (sem espaço se for nome composto): \n");
+    scanf("%s", cidade1);
 
     printf("Digite a população: \n");
-    scanf("%d", &populacao_1);
+    scanf("%d", &populacao1);
 
-    printf("Digite a Área: \n");
-    scanf("%f", &area_1);
+    printf("Digite a Área (em km²): \n");
+    scanf("%f", &area1);
 
     printf("Digite o PIB: \n");
-    scanf("%f", &pib_1);
+    scanf("%f", &pib1);
 
     printf("Digite o número de pontos turísticos: \n");
-    scanf("%d", &pontosturisticos_1);
+    scanf("%d", &pontosturisticos1);
 
+    printf("Os dados da primeira carta foram definidos!\n");
+    printf("Agora vamos para a segunda carta!\n");
+ 
     printf("***Escreva os dados da Carta 2***\n");
 
 
     printf("Digite a letra do Estado (Entre A e H): \n");
-    scanf("%s", &estado_2);
+    scanf("%c", &estado2);
 
     printf("Digite o código da carta: \n");
-    scanf("%s", &codigo_2);
+    scanf("%s", codigo2);
 
     printf("Digite o nome da cidade: \n");
-    scanf("%s", &cidade_2);
+    scanf("%s", cidade2);
 
     printf("Digite a população: \n");
-    scanf("%d", &populacao_2);
+    scanf("%d", &populacao2);
 
     printf("Digite a Área: \n");
-    scanf("%d", &area_2);
+    scanf("%f", &area2);
 
     printf("Digite o PIB: \n");
-    scanf("%f", &pib_2);
+    scanf("%f", &pib2);
+
+    printf("Digite o número de pontos turísticos: \n");
+    scanf("%d", &pontosturisticos2);
 
     //Dados da primeira carta.
     printf("Novo commit\n");
@@ -69,15 +92,30 @@ int main() {
 
     printf("\nCarta 1:\n");
 
-    printf("Estado:%c\n", estado_1);
-    printf("Código:%s\n", codigo_1);
-    printf("Cidade:%s\n", cidade_1);
-    printf("População:%i de habitantes\n", populacao_1);
-    printf("Área:%2fkm²\n", area_1);
-    printf("Pib:%2f bilhões de reais\n", pib_1);
-    printf("Número de Pontos Turísticos:%d\n", pontosturisticos_1);
+    printf("Estado: %c\n", estado1);
+    printf("Código: %s\n", codigo1);
+    printf("Cidade: %s\n", cidade1);
+    printf("População: %d de habitantes\n", populacao1);
+    printf("Área: %2f km²\n", area1);
+    printf("Pib: %2f bilhões de reais\n", pib1);
+    printf("Número de Pontos Turísticos: %d\n", pontosturisticos1);
 
     printf("Novo commit\n");
+
+    //Dados da segunda carta.
+
+    printf("\nCarta 2:\n");
+
+    printf("Estado: %c\n", estado2);
+    printf("Código: %s\n", codigo2);
+    printf("Cidade: %s\n", cidade2);
+    printf("População: %d de habitantes\n", populacao2);
+    printf("Área: %2f km²\n", area2);
+    printf("Pib: %2f bilhões de reais\n", pib2);
+    printf("Número de Pontos Turísticos: %d\n", pontosturisticos2);
+
+    return 0;
+
 
 
 
